8897abfa62264e89f592ed88bdd09f97b27f590b
Vo Son Ha <Vo Son Ha>
2017-04-15 17:13:23 +0700

    First commit to github

---
+++ Entity.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+
+namespace Pofsoft.DotNetCoreMongoContext
+{
+    /// <summary>
+    /// Entity, the other class will inheritance to use MongoContext
+    /// </summary>
+    public abstract class Entity
+    {
+        /// <summary>
+        /// _id key
+        /// </summary>
+        public ObjectId _id { get; set; }
+
+        /// <summary>
+        /// Constructor, _id will generate here
+        /// </summary>
+        public Entity()
+        {
+            _id = ObjectId.GenerateNewId();
+        }
+    }
+}
